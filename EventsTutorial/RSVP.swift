b2dd238dabf67a0a820541320ca7378e792b52e0
Alejandro <alejandrocarbaajal@gmail.com>
2018-10-31 14:41:08 -0600

    RSVP class has been created.

---
+++ EventsTutorial/RSVP.swift
@@ -0,0 +1,16 @@
+//
+//  RSVP.swift
+//  EventsApp
+//
+//  Created by Alejandro Carbajal on 10/31/18.
+//  Copyright © 2018 Alejandro Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class RSVP: NSObject {
+    
+    var title : String!
+    var yes : Int = 0
+    var no : Int = 0
+}
